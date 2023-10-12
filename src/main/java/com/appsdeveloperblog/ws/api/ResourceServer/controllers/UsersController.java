f643896ba4c1edcef437dd09c399f05cc11579c7
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-10-12 11:18:21 -0300

    reading UserId from JWT access token

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,11 +14,11 @@ public class UsersController {
         return "Working...";
     }
 
-    @PreAuthorize("hasAuthority('ROLE_developer')")
+    @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
     // @Secured("ROLE_developer")
     @DeleteMapping(path = "/{id}")
-    public String deleteUser(@PathVariable String id) {
-        return "Deleted user with id " + id;
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
     }
 
 }
